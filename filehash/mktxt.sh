dacd82ff972082524594cea0faf0f9dba8d8ed08
Richard van den Berg <richard@vdberg.org>
2020-12-05 10:30:22 +0100

    Create unique lists

--- filehash/mktxt.sh
+++ filehash/mktxt.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-find */sha256 -type f | sed -e 's#^.*/##' | sort > nioc-sha256.txt
-find */sha1   -type l | sed -e 's#^.*/##' | sort > nioc-sha1.txt
-find */md5    -type l | sed -e 's#^.*/##' | sort > nioc-md5.txt
+find */sha256 -type f | sed -e 's#^.*/##' | sort -u > nioc-sha256.txt
+find */sha1   -type l | sed -e 's#^.*/##' | sort -u > nioc-sha1.txt
+find */md5    -type l | sed -e 's#^.*/##' | sort -u > nioc-md5.txt
